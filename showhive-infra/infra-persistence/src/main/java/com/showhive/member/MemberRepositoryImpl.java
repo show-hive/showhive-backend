6bb333f55a7c90341fc71f6aceca382bf7a6d7c4
Jiihyun <cwlgusc@gmail.com>
2025-09-01 21:34:39 +0900

    chore: 삭제 예정 메서드 주석 추가

--- showhive-infra/infra-persistence/src/main/java/com/showhive/member/MemberRepositoryImpl.java
+++ showhive-infra/infra-persistence/src/main/java/com/showhive/member/MemberRepositoryImpl.java
@@ -2,25 +2,25 @@ package com.showhive.member;
 
 import com.showhive.exception.member.MemberErrorCode;
 import com.showhive.exception.member.MemberException;
-import org.springframework.stereotype.Repository;
 import com.showhive.member.domain.Member;
 import com.showhive.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepositoryImpl implements MemberRepository {
 
     private final JpaMemberRepository jpaMemberRepository;
 
-    public MemberRepositoryImpl(JpaMemberRepository jpaMemberRepository) {
-        this.jpaMemberRepository = jpaMemberRepository;
-    }
-
     @Override
+    // TODO: 사용 안 할 시 삭제 예정
     public void signUp(Member member) {
         jpaMemberRepository.save(member);
     }
 
     @Override
+    // TODO: 사용 안 할 시 삭제 예정
     public Member findById(long memberId) {
         return jpaMemberRepository.findById(memberId)
                 .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
