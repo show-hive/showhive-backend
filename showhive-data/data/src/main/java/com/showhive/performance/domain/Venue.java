1ab820523653839db9cdbf94ba969efccbca9adf
Jiihyun <cwlgusc@gmail.com>
2025-09-08 22:09:55 +0900

    feat: Venue 엔티티 생성

---
+++ showhive-data/data/src/main/java/com/showhive/performance/domain/Venue.java
@@ -0,0 +1,39 @@
+package com.showhive.performance.domain;
+
+import com.showhive.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "venues")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Venue extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    private String address;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private String contactNumber;
+
+    @NotBlank
+    private String link;
+}
