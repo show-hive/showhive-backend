cfe95a34dcc232a4aa61be380e2eb43a8d329a78
Jiihyun <cwlgusc@gmail.com>
2025-09-08 22:24:59 +0900

    feat: Seat 엔티티 생성

---
+++ showhive-data/data/src/main/java/com/showhive/performance/domain/Seat.java
@@ -0,0 +1,44 @@
+package com.showhive.performance.domain;
+
+import com.showhive.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "seats")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Seat extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "venue_id")
+    private Venue venue;
+
+    @Column(name = "`column`")
+    private String column;
+
+    @Column(name = "`row`")
+    private Integer row;
+
+    private Integer floor;
+
+    @NotBlank
+    private String seatType;
+}
