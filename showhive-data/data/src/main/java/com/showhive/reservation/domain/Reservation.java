86e79657a7cffdd77c0e98d087309c9852a463c4
Jiihyun <cwlgusc@gmail.com>
2025-09-09 12:58:44 +0900

    feat: Reservation 엔티티 생성

--- showhive-data/data/src/main/java/com/showhive/reservation/domain/Reservation.java
+++ showhive-data/data/src/main/java/com/showhive/reservation/domain/Reservation.java
@@ -1,6 +1,7 @@
-package com.showhive.performance.domain;
+package com.showhive.reservation.domain;
 
 import com.showhive.BaseEntity;
+import com.showhive.member.domain.Member;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -14,34 +15,28 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
-@Table(name = "performance_seats")
+@Table(name = "reservations")
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class PerformanceSeat extends BaseEntity {
+public class Reservation extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-// TODO: PerformanceSession 연결
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    // TODO: Code 연결
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "code_id")
+//    private Code reservationStatusCode;
+
+    // TODO: PerformanceSession 연결
 //    @ManyToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "session_id")
 //    private PerformanceSession performanceSession;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "seat_id")
-    private Seat seat;
-
-// TODO: Code 연결
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "code_id")
-//    private Code seatStatusCode;
-
-    private int price;
-
-    private LocalDateTime holdExpiresAt;
 }
