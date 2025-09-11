abfe5b978ad181be9f11e86e6219bab8d5a5f55b
beetrbgus <devbeetrb0214@gmail.com>
2025-09-12 00:19:32 +0900

    feat : 모듈 추가 및 boot모듈명 api로 변경

---
+++ showhive-boot/admin/src/main/java/com/showhive/admin/infrastructure/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.showhive.admin.infrastructure;
+
+import org.springframework.context.annotation.Configuration;
+
+//TIP 해당 부분은
+// ㅇㅇ
+@Configuration
+public class SecurityConfig {
+    //TODO 회원 인증이 처리 완료 되었을 때 현재 showhive-boot/api에 있는 security config의 admin 설정은 해당 부분으로 가져와야 함.
+}
