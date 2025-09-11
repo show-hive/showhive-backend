d1a5cb79d27f4c1109ac355bc3fb5576c07ca843
beetrbgus <devbeetrb0214@gmail.com>
2025-09-11 23:44:55 +0900

    feat : jpa 삭제

--- showhive-data/data/src/main/java/com/showhive/file/repository/FileRepository.java
+++ showhive-data/data/src/main/java/com/showhive/file/repository/FileRepository.java
@@ -3,5 +3,5 @@ package com.showhive.file.repository;
 import com.showhive.file.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileRepository extends JpaRepository<File, Long> {
+public interface FileRepository {
 }
