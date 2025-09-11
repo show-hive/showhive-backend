3c07c5667d79f196df5b3bd6a47fbfd5a15f070d
beetrbgus <devbeetrb0214@gmail.com>
2025-09-11 22:29:46 +0900

    feat : repository 삭제

--- showhive-data/data/src/main/java/com/showhive/performance/repository/CastingRepository.java
+++ showhive-data/data/src/main/java/com/showhive/performance/repository/CastingRepository.java
@@ -1,7 +1,4 @@
 package com.showhive.performance.repository;
 
-import com.showhive.performance.domain.Casting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CastingRepository extends JpaRepository<Casting, Long> {
+public interface CastingRepository {
 }
--- showhive-data/data/src/main/java/com/showhive/performance/repository/PerformanceSessionRepository.java
+++ showhive-data/data/src/main/java/com/showhive/performance/repository/PerformanceSessionRepository.java
@@ -1,7 +1,4 @@
 package com.showhive.performance.repository;
 
-import com.showhive.performance.domain.PerformanceSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerformanceSessionRepository extends JpaRepository<PerformanceSession, Long> {
+public interface PerformanceSessionRepository {
 }
