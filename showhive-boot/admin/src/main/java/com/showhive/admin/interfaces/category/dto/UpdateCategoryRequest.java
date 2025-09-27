d977626476e609962abd921ca94fc46db269e5a5
beetrbgus <devbeetrb0214@gmail.com>
2025-09-27 15:16:46 +0900

    feat : 문서 추가

--- showhive-boot/admin/src/main/java/com/showhive/admin/interfaces/category/dto/UpdateCategoryRequest.java
+++ showhive-boot/admin/src/main/java/com/showhive/admin/interfaces/category/dto/UpdateCategoryRequest.java
@@ -1,18 +1,31 @@
 package com.showhive.admin.interfaces.category.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record UpdateCategoryRequest(
-        @NotNull @NotBlank
+        @Schema(name = "그룹 코드", example = "MENU", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank
         String groupCode,
+
+        @Schema(name = "부모 카테고리 ID", example = "1", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
         Long parentId,
-        @NotNull @NotBlank
+
+        @Schema(name = "값", example = "홈", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank
         String value,
+
+        @Schema(name = "카테고리에 대한 설명", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
         @NotBlank
         String description,
+
+        @Schema(name = "깊이", example = "0", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
         Short level,
+
+        @Schema(name = "깊이", example = "0", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
         Short sortOrder,
+
+        @Schema(name = "사용 여부", example = "true", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
         Boolean isActive
 ) {
 }
