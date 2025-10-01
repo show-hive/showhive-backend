405d1c7385c69fb13593c070fb9423c23a145e31
whitejh <jhk0806@gmail.com>
2025-10-01 18:49:32 +0900

    refactor : 도메인 모듈에 있는 repository 클래스에는 어노테이션 없이 메서드만 선언

--- showhive-domain/domain/src/main/java/com/showhive/coupon/repository/query/CouponInfoQueryRepository.java
+++ showhive-domain/domain/src/main/java/com/showhive/coupon/repository/query/CouponInfoQueryRepository.java
@@ -13,8 +13,6 @@ public interface CouponInfoQueryRepository {
 
     // 쓰기 락, 비관적 락 (동시성 문제 방지)
     // 다수의 사용자가 동시에 쿠폰 정책의 재고를 감소시키는 경우에 사용
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("SELECT ci FROM CouponInfo ci where ci.id=:id")
     Optional<CouponInfo> findByIdWithLock(Long id);
 
     List<CouponInfo> getAll();
--- showhive-domain/domain/src/main/java/com/showhive/coupon/repository/query/CouponQueryRepository.java
+++ showhive-domain/domain/src/main/java/com/showhive/coupon/repository/query/CouponQueryRepository.java
@@ -19,7 +19,6 @@ public interface CouponQueryRepository {
     Optional<Coupon> findByIdAndMemberId(Long id, Long memberId);
 
     // 특정 정책id로 발급된 쿠폰의 개수를 확인
-    @Query("SELECT COUNT(c) FROM Coupon c WHERE c.couponInfo.id = :infoId")
     Long countByCouponInfoId(@Param("infoId") Long infoId);
 
     // 특정 유저가 최근에 발급받은 쿠폰 리스트를 가지고 오는 인터페이스
@@ -27,7 +26,5 @@ public interface CouponQueryRepository {
 
     // 쓰기 락, 비관적 락 (동시성 문제 방지)
     // 다수의 사용자가 동시에 쿠폰 정책의 재고를 감소시키는 경우에 사용
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("SELECT c FROM Coupon c where c.id=:id")
     Optional<Coupon> findByIdWithLock(Long id);
 }
